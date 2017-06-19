71d0c8047d5df832e66a86063d24d5e6aeba5226
wuxp <windfishing5@gmail.com>
2017-06-19 21:29:00 +0800

    all

---
+++ L06/软件开发规范/ATM/bin/start.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+#Author:xp
+#blog_url: http://blog.csdn.net/wuxingpu5/article/details/71209731
+
+import os,sys
+
+BASE_DIR=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+
+from core import main
+if __name__ == '__main__':
+    main.run()
