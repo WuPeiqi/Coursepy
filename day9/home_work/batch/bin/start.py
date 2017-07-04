948b8ddcb15ace697eeefc40288ca20fc709b1ad
wuxp <windfishing5@gmail.com>
2017-07-04 11:10:00 +0800

    add

--- day9/home_work/batch/bin/start.py
+++ day9/home_work/batch/bin/start.py
@@ -6,27 +6,29 @@
 
 import paramiko
 import configparser
+from multiprocessing import Pool,Process
 
 conffile='a.ini'
 
 config=configparser.ConfigParser()
 config.read(conffile)
-print(config.sections()) #标题
-print(config.options('global')) #key
-print(config.items('group'))#key value
-print(config.get('h1','ip'))  #标题下 键对应的值
+#print(config.sections()) #标题
+#print(config.options('global')) #key
+#print(config.items('group'))#key value
+#print(config.get('h1','ip'))  #标题下 键对应的值
 
 def info_init(hostname):
-    ip=''
-    port=''
-    user=''
-    passwd=''
+    ip=config.get(hostname,'ip')
+    port=config.get(hostname,'port')
+    user=config.get(hostname,'user')
+    passwd=config.get(hostname,'passwd')
+    print('%s host_info'%(hostname),ip,port,user,passwd)
     return ip,port,user,passwd
 
-def ssh_conn(ip,port,user,passwd,command):
+def ssh_conn(login,command):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(ip,port,user,passwd)
+    ssh.connect(login[0],int(login[1]),login[2],login[3])
     stdin,stdout,stderr = ssh.exec_command(command)
     print(stdout.read().decode('utf-8'))
     ssh.close()
@@ -42,3 +44,13 @@ class ftp_conn():
         sftp.close()
     def ftp_get(self):
         pass
+if __name__ == '__main__':
+    #info_init('h1')        测试可以正常读取到主机名对应的信息,下面使用回调函数
+    # pool=Pool()
+    # p
+    # res=info_init('h1')
+    # print(type(res))
+    # print(res[1])
+    hostname=input('请输入主机名')
+    command=input('请输入执行的命令')
+    ssh_conn(info_init(hostname),command)
--- day9/home_work/readme.txt
+++ day9/home_work/readme.txt
@@ -1,2 +1,3 @@
 #blog_url: http://blog.csdn.net/wuxingpu5/article
 主程序 bin start a.ini配置文件
+执行 start.py  输入主机名h1 输入执行的命令 则返回执行结果
